86f0d2429edf2936aeb01ed2e676d05b49faabb1
SRV <97838319+vikneshsrv24@users.noreply.github.com>
2025-05-01 21:40:32 +0530

    added main menu, view habits
    created function to view habits, created main menu which is right now built using numbers to select options.
    
    need to create more refining and functioning code as well as want to write summary function

--- main.py
+++ main.py
@@ -3,7 +3,23 @@ def add_habits():
     habits.append(habit)
     return habits
 
+def view_habits():
+    print(habits)
+
 
 habits=[]
 status={}
-print(add_habits())
+#print(add_habits())
+loop=1
+while loop>0:
+    print(" 1.Add Habits" \
+    " 2.View Habits" \
+    " 3.Summary")
+    option=int(input("What you wish to do [select from the options] ?"))
+    if option==1:
+        add_habits()
+    elif option==2:
+        print(view_habits())
+    else:
+        print(" Summary function")
+        loop=0
