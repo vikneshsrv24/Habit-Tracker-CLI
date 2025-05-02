69116f0810f711c68749cf4bb277b147650dd7b8
SRV <97838319+vikneshsrv24@users.noreply.github.com>
2025-05-02 20:16:34 +0530

    added few functions
    1.changed the error of getting none type and refined the view_habits.
    2.should add the summarizing and status function asap

--- main.py
+++ main.py
@@ -1,26 +1,45 @@
+# Funtion to Add habits
 def add_habits():
     habit=input("Enter you habit: ")
     habits.append(habit)
-    print(f"{habits} added successfully")
+    print(f"{','.join(habits)} added successfully")      # to remove the brackets of the array
+    morehabit=input("Do you want to include more habits !: ")
+    if morehabit =='y':
+        add_habits()
+    else:
+        return
 
+# TODO : Remove none type from the loop.
 def view_habits():
-    for i in habits:
+    for i in (habits):     
         print(i)
 
+# TODO : Create a function that can summarize the habits.
+# TODO : Create a function to mark the habits that is done. 
+habits_type = {
 
+}
+ 
 habits=[]
 status={}
-#print(add_habits())
 loop=1
 while loop>0:
-    print(" 1.Add Habits" \
-    " 2.View Habits" \
-    " 3.Summary")
-    option=int(input("What you wish to do [select from the options] ?"))
+    print("1. Add Habits")
+    print("2. View Habits")
+    print("3.Checklist")   
+    print("4. View Summary")        
+    option=int(input("What you wish to do ? [select from the options] : "))
     if option==1:
         add_habits()
     elif option==2:
-        print(view_habits())
+        view_habits()
+    elif option==3:
+        for i in habits:
+            print("Type '1' if the habit is completed or '0' for pending list ")
+            history=print(i).int(input(' : Completed or Pending : '))
+            # if history ==1:
+            #     status+=i
+            # else:
     else:
-        print(" Summary function")
+        print("Not available input. Please run again !!!")
         loop=0
