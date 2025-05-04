86b7ae98e3195bef2330ae0f9e7c94a308e2813e
SRV <97838319+vikneshsrv24@users.noreply.github.com>
2025-05-04 15:05:03 +0530

    Added habits to json
    Added json file by writing habits snd status to a json file. So the memory is saved even when the app restarts.

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
+import json
+import os
+
+# Loading previous data from JSON file if exists
+if os.path.exists("habits.json"):
+    with open("habits.json","r") as f:
+        data=json.load(f)
+        habits=data.get("habits",[])
+        status=data.get("status",{})
+else:
+    habits=[]
+    status={}
+
 # Funtion to Add habits
 def add_habits():
     habit=input("Enter you habit: ")
     habits.append(habit)
+    status[habit]=0
+    save_data()
     print(f"{','.join(habits)} added successfully")      # to remove the brackets of the array
-    morehabit=input("Do you want to include more habits !: ")
+    morehabit=input("Do you want to include more habits(y/n) !: ")
     if morehabit =='y':
         add_habits()
-    else:
-        return
+   
+    
 
 # Function to view habits
 def view_habits():
@@ -29,18 +44,30 @@ def update_status():
     for habit in habits:
         state=input(f"Did you complete {habit} today ? (type '1' for completed or '0' for not): ")
         status[habit]=int(state)
+    save_data()
 
-status={}
+# Saving data into json
+def save_data():
+    with open("habits.json","w") as f:
+        json.dump({"habits":habits,"status":status},f,indent=4)
+
+# Clear saved JSON memory
+def clear_memory():
+    habits.clear()
+    status.clear()
+    save_data()
+    print("Memory cleared !")
 
 # Main menu
-habits=[]
+
 loop=1
 while loop>0:
     print('\n'*1)
     print("1. Add Habits")
     print("2. View Habits")
     print("3. Update Status")   
-    print("4. View Summary\n")        
+    print("4. View Summary")
+    print("5. Clear Saved Habits\n")        
     option=int(input("What you wish to do ? [Type number from the options] : "))
     if option==1:
         add_habits()
@@ -54,6 +81,9 @@ while loop>0:
     elif option==4:
         summary()
     
+    elif option==5:
+        clear_memory()
+
     else:
         print("Not available input. Please run again !!!")
         loop=0
